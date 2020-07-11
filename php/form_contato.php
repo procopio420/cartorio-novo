1d5df3bdae8845632242ca5b162e0a1222ac35f6
procopio420 <lucas-procopio@outlook.com>
2020-07-11 17:17:09 -0300

    pronto

--- php/form_contato.php
+++ php/form_contato.php
@@ -1,4 +1,12 @@
 <?php
+function console_log($output, $with_script_tags = true) {
+  $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+  if ($with_script_tags) {
+      $js_code = '<script>' . $js_code . '</script>';
+  }
+  echo $js_code;
+}
 # Put your config
 $emailTo = "contato@9oficioniteroi.com";
 $vname = "Cartorio 9 Oficio Niteroi";
@@ -23,13 +31,15 @@ Telefone: ' . $telefone . '
  
 mail(utf8_decode($emailTo), utf8_decode($subject), utf8_decode($message), utf8_decode($email)."\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\n");
  
-
+console_log($name, $email, $telefone)
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
 echo "<script type='text/javascript'> 
 alert('Insira um email válido.'); 
 </script>"; 
 echo "<script>location.href='contato.html'</script>"; // Página que será redirecionada
+console_log('if')
+
 }
 
 
@@ -38,8 +48,9 @@ else{
 echo "<script type='text/javascript'> 
 alert('Sua mensagem foi enviada com sucesso!'); 
 </script>"; 
-echo "<script>window.history.back();</script>"; // Página que será redirecionada
 
+echo "<script>window.history.back();</script>"; // Página que será redirecionada
+console_log('else')
 }
 
 ?>
