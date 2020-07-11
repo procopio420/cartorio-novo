2509e78b78a09b3952cebdce5a1e43a5bc58a7c2
procopio420 <lucas-procopio@outlook.com>
2020-07-11 17:23:34 -0300

    blackinkgalery

--- php/form_contato.php
+++ php/form_contato.php
@@ -1,12 +1,4 @@
 <?php
-function console_log($output, $with_script_tags = true) {
-  $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-');';
-  if ($with_script_tags) {
-      $js_code = '<script>' . $js_code . '</script>';
-  }
-  echo $js_code;
-}
 # Put your config
 $emailTo = "contato@9oficioniteroi.com";
 $vname = "Cartorio 9 Oficio Niteroi";
@@ -31,15 +23,13 @@ Telefone: ' . $telefone . '
  
 mail(utf8_decode($emailTo), utf8_decode($subject), utf8_decode($message), utf8_decode($email)."\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\n");
  
-console_log($name, $email, $telefone)
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
 echo "<script type='text/javascript'> 
 alert('Insira um email válido.'); 
 </script>"; 
 echo "<script>location.href='contato.html'</script>"; // Página que será redirecionada
-console_log('if')
-
+echo "<script>console.log('if')</script>";
 }
 
 
@@ -50,7 +40,7 @@ alert('Sua mensagem foi enviada com sucesso!');
 </script>"; 
 
 echo "<script>window.history.back();</script>"; // Página que será redirecionada
-console_log('else')
+echo "<script>console.log('else')</script>";
 }
 
 ?>
